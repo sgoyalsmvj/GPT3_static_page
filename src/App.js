519097487b6b93203bdf94a66c5f709428e8837c
Saurabh Goyal <sgoyalsmvj@gmail.com>
2023-05-17 14:27:01 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css';
+import { Footer,Blog,Possibility,Features,WhatGPT3,Header } from './containers';
+import { CTA,Article,Brand,Navbar,Feature } from './components';
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
